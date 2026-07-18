38f1aed477cdbdd066f7ac018d55bf922e81e69a
rckwd <key.grunge@gmail.com>
2026-07-18 19:41:03 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,2 @@
-const CACHE = 'pembukuan-v1';
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html', '/style.css', '/app.js'])));
-});
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+self.addEventListener('install', e => e.waitUntil(caches.open('srf-v1').then(c => c.addAll(['/', '/index.html', '/style.css', '/app.js']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
